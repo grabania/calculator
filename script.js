c8732ca0d72ac445f2580065dd03ae032d893987
grabania <grabania.work@gmail.com>
2020-05-16 19:44:43 +0100

    Create operator section  in JS

--- script.js
+++ script.js
@@ -26,3 +26,9 @@ function getFormattedNumber(num) {
 function reverseNumberFormat(num) {
 	return Number(num.replace(/,/g, ''));
 }
+var operator = document.getElementsByClassName('operator');
+for (var i = 0; i < operator.length; i++) {
+	operator[i].addEventListener('click', function() {
+		alert('The operator clicked:' + this.id);
+	});
+}
